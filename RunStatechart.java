04e2a97a0f29c44c863eee816f9951123a48a7c1
xVictoryerx <37668899+xVictoryerx@users.noreply.github.com>
2021-03-14 21:21:41 +0100

    3rd task finished

--- RunStatechart.java
+++ RunStatechart.java
@@ -2,30 +2,52 @@ package hu.bme.mit.yakindu.analysis.workhere;
 
 import java.io.IOException;
 
-// import hu.bme.mit.yakindu.analysis.RuntimeService;
-// import hu.bme.mit.yakindu.analysis.TimerService;
+import hu.bme.mit.yakindu.analysis.RuntimeService;
+import hu.bme.mit.yakindu.analysis.TimerService;
 import hu.bme.mit.yakindu.analysis.example.ExampleStatemachine;
 import hu.bme.mit.yakindu.analysis.example.IExampleStatemachine;
 
+import java.io.InputStreamReader; 
+import java.io.BufferedReader;
 
 
 public class RunStatechart {
 	
 	public static void main(String[] args) throws IOException {
-//		ExampleStatemachine s = new ExampleStatemachine();
-//		s.setTimer(new TimerService());
-//		RuntimeService.getInstance().registerStatemachine(s, 200);
-//		s.init();
-//		s.enter();
-//		s.runCycle();
-//		print(s);
-//		s.raiseStart();
-//		s.runCycle();
-//		System.in.read();
-//		s.raiseWhite();
-//		s.runCycle();
-//		print(s);
-//		System.exit(0);
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		ExampleStatemachine s = new ExampleStatemachine();
+		s.setTimer(new TimerService());
+		RuntimeService.getInstance().registerStatemachine(s, 200);
+		s.init();
+		s.enter();
+		s.runCycle();
+		boolean run = true;
+		while(run) {
+			String command = reader.readLine();
+			switch(command) {
+				case "start":
+					s.raiseStart();
+					s.runCycle();
+					break;
+				case "black":
+					s.raiseBlack();
+					s.runCycle();
+					break;
+				case "white":
+					s.raiseWhite();
+					s.runCycle();
+					break;
+				case "exit":
+					run = false;
+					break;
+				default:
+					System.out.println("Unrecognised command.");
+					break;
+			}
+			print(s);
+		}
+		reader.close();
+		System.exit(0);
 	}
 
 	public static void print(IExampleStatemachine s) {
