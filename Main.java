80c5e1cbd2e94aee5c3108b146ace5dfdfb1b01a
xVictoryerx <37668899+xVictoryerx@users.noreply.github.com>
2021-03-14 19:46:46 +0100

    2nd task done

--- Main.java
+++ Main.java
@@ -1,10 +1,13 @@
 package hu.bme.mit.yakindu.analysis.workhere;
 
+import java.util.ArrayList;
+
 import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.ecore.EObject;
 import org.junit.Test;
 import org.yakindu.sct.model.sgraph.State;
 import org.yakindu.sct.model.sgraph.Statechart;
+import org.yakindu.sct.model.sgraph.Transition;
 
 import hu.bme.mit.model2gml.Model2GML;
 import hu.bme.mit.yakindu.analysis.modelmanager.ModelManager;
@@ -25,13 +28,35 @@ public class Main {
 		// Reading model
 		Statechart s = (Statechart) root;
 		TreeIterator<EObject> iterator = s.eAllContents();
+		ArrayList<State> trapNodes = new ArrayList<State>();
+		ArrayList<State> anonymNodes = new ArrayList<State>();
 		while (iterator.hasNext()) {
 			EObject content = iterator.next();
 			if(content instanceof State) {
 				State state = (State) content;
+				if(state.getOutgoingTransitions().size() == 0)trapNodes.add(state);
+				if(state.getName() == "")anonymNodes.add(state);
 				System.out.println(state.getName());
 			}
+			if(content instanceof Transition) {
+				Transition transition = (Transition) content;
+				System.out.println(transition.getSource().getName() + " -> " + transition.getTarget().getName());
+			}
 		}
+		if(trapNodes.size() > 0) {
+			System.out.println("The following states do not have outgoing transitions:");
+			for(int i = 0; i < trapNodes.size(); ++i) {
+				System.out.println(trapNodes.get(i).getName());
+			}
+		}
+		else System.out.println("All states have outgoing transitions.");
+		if(anonymNodes.size() > 0) {
+			System.out.println( "There are " + anonymNodes.size() + " states that lack names. Proposal:");
+			for(int i = 0; i < anonymNodes.size(); ++i) {
+				System.out.println("Anonymus_" + i);
+			}
+		}
+		else System.out.println("All states have names.");
 		
 		// Transforming the model into a graph representation
 		String content = model2gml.transform(root);
